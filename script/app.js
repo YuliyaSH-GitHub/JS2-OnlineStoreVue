88ff41d99c182071d45db9040a990345b58f2b1b
Yuliya <shishova-yg@yandex.ru>
2021-06-15 18:54:40 +0300

    написаны методы добавления товаров в корзину

--- script/app.js
+++ script/app.js
@@ -6,8 +6,14 @@ const app = new Vue({
         basketItems: [],
         isVisibleCart: false,
         searchLine: '',
+        totalSum: null,
     },
     methods: {
+        /**
+         * Метод получает товары в формате json с локального сервера
+         * @param {url} url  url-адрес catalogData.json
+         * @returns Promise
+         */
         fetchGoods(url) {
             return fetch(url)
                 .then(result => result.json())
@@ -15,21 +21,61 @@ const app = new Vue({
                     console.log(error);
                 })
         },
+        /**
+         * Метод производит фильтрацию товаров на странице по введенному в input наименованию
+         */
         filterGoods() {
             const regexp = new RegExp(this.searchLine, 'i');
             this.goods = this.goods.filter(product => regexp.test(product.title));
         },
-
-        addProduct(product) {
-
-            console.log(product.id);
+        /**
+         * Метод добавляет товары в массив корзины товаров basketItems
+         * @param {id: string, title: string, price: string} item 
+         */
+        addProduct(item) {
+            let findItem = this.basketItems.find(elem => elem.id === item.id);
+            if (findItem) {
+                findItem.count++;
+            } else {
+                const productItem = Object.assign({
+                    count: 1
+                }, item);
+                this.basketItems.push(productItem);
+            }
+            this.getTotalSum();
         },
+        /**
+         * Метод удаляет товары из корзины при клике на кнопку класса productRemoveGoods
+         * @param {id: string, title: string, price: string, count: number,} item 
+         * @returns this.basketItems - массив товаров корзины
+         */
+        removeProduct(item) {
+            if (item.count > 1) {
+                item.count--;
+            } else {
+                this.basketItems.splice(this.basketItems.indexOf(item), 1);
+            }
+            this.getTotalSum();
+
+        },
+        getTotalSum() {
+            let sum = 0;
+            this.basketItems.forEach(item => {
+                sum += item.price * item.count;
+            })
+            this.totalSum = sum;
+        },
+        /**
+         * Метод выводит сообщение, если список товаров пуст (json) 
+         */
         absentData() {
             alert('Нет данных');
-        }
+        },
     },
     mounted() {
-
+        /**
+         * Метод формирует массив товаров this.goods
+         */
         this.fetchGoods(`${API}/catalogData.json`)
             .then(data => {
                 if (data == undefined) {
@@ -40,6 +86,5 @@ const app = new Vue({
                 }
                 console.log(this.goods);
             })
-
     }
 })
