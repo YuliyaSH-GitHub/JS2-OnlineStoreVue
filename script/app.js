a6568bb0daa1f7dd85999c6ae8e76926d6a6d4c9
Yuliya <shishova-yg@yandex.ru>
2021-06-15 15:30:35 +0300

    написан метод filterGoods() - фильтр товаров на странице

--- script/app.js
+++ script/app.js
@@ -1,22 +1,40 @@
-const API = "http://JS2-OnlineStoreVue/"; //файл json хранится на локальном сервере
+const API = "http://js2-onlinestorevue/"; //файл json хранится на локальном сервере
 const app = new Vue({
     el: '#app',
     data: {
         goods: [],
+        basketItems: [],
         isVisibleCart: false,
+        searchLine: '',
     },
     methods: {
-        fetchGoods() {
+        fetchGoods(url) {
             return fetch(url)
                 .then(result => result.json())
                 .catch(error => {
                     console.log(error);
                 })
+        },
+        filterGoods() {
+            const regexp = new RegExp(this.searchLine, 'i');
+            this.goods = this.goods.filter(product => regexp.test(product.title));
+        },
+
+        addProduct(product) {
+
+            console.log(product.id);
+        },
+        absentData() {
+            alert('Нет данных');
         }
     },
     mounted() {
+
         this.fetchGoods(`${API}/catalogData.json`)
             .then(data => {
+                if (data == undefined) {
+                    this.absentData();
+                }
                 for (let item of data) {
                     this.goods.push(item);
                 }
